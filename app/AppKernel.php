70a2a16779246229bb40c3b35cdc71e2230c02e2
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-29 16:14:22 -0400

    Enabling HarvestCloud\CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new HarvestCloud\CoreBundle\HarvestCloudCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
