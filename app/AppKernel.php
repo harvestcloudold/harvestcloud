3de67acbc9fd21be163aa2fc755bc6d943d9f21a
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-30 15:01:14 -0400

    Enabling and configuring Stof\DoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new HarvestCloud\CoreBundle\HarvestCloudCoreBundle(),
             new HarvestCloud\GeoBundle\HarvestCloudGeoBundle(),
             new HarvestCloud\UserBundle\HarvestCloudUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
         );
 
