36188c182e47bbb8a0284ea559ef0fa504f8f896
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-31 20:38:18 -0400

    Enabling HarvestCloud\GeoBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new HarvestCloud\DemoBundle\HarvestCloudDemoBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new HarvestCloud\GeoBundle\HarvestCloudGeoBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
